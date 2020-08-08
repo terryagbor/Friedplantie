7f7e4deea239e24ad6ff7484b86b3ffa92255442
terryagbor <terryagbor@gmail.com>
2020-08-07 22:05:03 -0600

    second assignment

---
+++ Friedplantie2.py
@@ -0,0 +1,32 @@
+Friedplantie
+#Stage 1: input for fried plantain
+ripe plantain = 3
+vegetable oil = 2 litres
+salt = 1/2 teaspoon
+knife = sharp
+bowl = deep
+fork = 1
+fryer = deep
+serving plate = 1
+cooking spoon = 1
+paper towel = 5 pieces
+garbage bags = 2
+stove =  electric
+water = tap
+#2 Stage 2 - Process
+Get 3 riped plantains from kitchen cabinet and place on floor
+use the sharp knife to peel the plantains 
+put the peelings into the garbage bag 
+chop the plantain into tiny slizes, cutting longitudinally each time
+put the chopped plantain into the deep bowl
+add the salt into the chopped plantain and mix gently
+put the oil into the deep fryer and turn it on
+wait for the oil to get hot and add the chopped plantain
+stir periodially with the cooking spoon until its ready
+transfer the deep fried plantain into the serving plate
+turn off the deep fryer
+wash your hands using runnung tap water
+dry them with paper towel
+#stage 3: output
+using a fork, eat the prepared plantains
+enjoy!
