2766a3b8b3c01cd187a0d4e188015f54907c5f0a
terryagbor <terryagbor@gmail.com>
2020-08-12 22:08:39 -0600

    Variable

---
+++ snack.py
@@ -0,0 +1,12 @@
+favorite_snack = 'puff_puff'
+print(favorite_snack)
+print(favorite_snack*100)
+Senge_favoritesnack = 'gateau'
+print(Senge_favoritesnack)
+print(Senge_favoritesnack*100)
+grocery_items = ['toilet_tissue', 'soap', 'milk', 'bread', 'cheese']
+print(grocery_items)
+grocery_items.count(Senge_favoritesnack)
+favorite_snack, Senge_favoritesnack = Senge_favoritesnack, favorite_snack
+print(Senge_favoritesnack)
+print(favorite_snack)
